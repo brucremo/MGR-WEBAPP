ebeb7e9ae4237e90e122804abe412895af39cad2
jsidhu26 <37817565+jsidhu26@users.noreply.github.com>
2019-03-19 00:48:49 -0400

    ADDED: group page button functionality and formatting

--- src/app/group-page/group-page.component.ts
+++ src/app/group-page/group-page.component.ts
@@ -14,63 +14,84 @@ export class GroupPageComponent implements OnInit {
   public group: any;
   public userID: String;
   public isMember: boolean;
+  public isAdmin: boolean;
+  public isModerator: boolean;
+  public isOwner: boolean;
+  public askedToJoin: boolean;
 
   constructor(private router: Router,
     private r: ActivatedRoute,
-    private api: ApiService) { 
-      this.group = {};  
-      this.userID = "";
-      this.isMember = false;
-    }
+    private api: ApiService) {
+    this.group = {};
+    this.userID = "";
+    this.isMember = false;
+    this.isAdmin = false;
+    this.isModerator = false;
+    this.isOwner = false;
+    this.askedToJoin = false;
+  }
 
   ngOnInit() {
-    //get the group info
-    
+    //get the group from the API
     //extract the group id from the url
     var url = window.location.href;
     var groupID = {
-      "GROUPID": url.slice(url.lastIndexOf('/')+1)
+      "GROUPID": url.slice(url.lastIndexOf('/') + 1)
     };
     console.log("group ID: " + groupID.GROUPID);
     //pass it as a parameter to the relevant API function
     //subscribe to the response given by the API
-    this.api.getGroup(groupID).subscribe(res =>{
+    this.api.getGroup(groupID).subscribe(res => {
       console.log(res);
       this.group = res;
 
       //format the received object's date
-      this.group[0].GROUPID = this.group[0].GROUPID.toUpperCase(); 
+      this.group[0].GROUPID = this.group[0].GROUPID.toUpperCase();
       this.group[0].GROUPCREATIONDATE = new Date(this.group[0].GROUPCREATIONDATE).toLocaleDateString("en-US");
 
       //check if user is a part of the group
-      if(document.cookie){
-        this.userID = document.cookie.split("=")[1];
-        
+      if (document.cookie) {
+        this.userID = "brucremo";//document.cookie.split("=")[1];
+
         //compare userID with owner
-        if(this.userID == this.group[0].GROUPOWNER)
+        if (this.userID == this.group[0].GROUPOWNER) {
           this.isMember = true;
+          this.isOwner = true;
+        }
 
         //compare userID with admins
-        for(var i = 0; i < this.group[0].GROUPADMINS.length; i++){
-          if(this.userID == this.group[0].GROUPADMINS[i].USERID)  
-            this.isMember = true; 
+        for (var i = 0; i < this.group[0].GROUPADMINS.length; i++) {
+          if (this.userID == this.group[0].GROUPADMINS[i].USERID) {
+            this.isMember = true;
+            this.isAdmin = true;
+          }
         }
 
         //compare userID with moderators
-        for(var i = 0; i < this.group[0].GROUPMODERATORS.length; i++){
-          if(this.userID == this.group[0].GROUPMODERATORS[i].USERID)
+        for (var i = 0; i < this.group[0].GROUPMODERATORS.length; i++) {
+          if (this.userID == this.group[0].GROUPMODERATORS[i].USERID) {
             this.isMember = true;
+            this.isModerator = true;
+          }
         }
 
         //compare userID with other members
-        for(var i = 0; i < this.group[0].GROUPMEMBERS.length; i++){
-          if(this.userID == this.group[0].GROUPMEMBERS[i].USERID)
+        for (var i = 0; i < this.group[0].GROUPMEMBERS.length; i++) {
+          if (this.userID == this.group[0].GROUPMEMBERS[i].USERID)
             this.isMember = true;
         }
       }
-    }, err =>{
+    }, err => {
       console.log("error: " + err);
       this.router.navigate(['/404']);
     })
+  }
+
+  onJoinGroup() {
+    this.askedToJoin = true;
+  }
+
+  onCancelJoinRequest(){
+    this.askedToJoin=false;
   }
 }
