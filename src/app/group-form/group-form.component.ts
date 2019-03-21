0e9e8fb33e8e5220de553dc25b29c2874f8ec24d
Jaideep Sidhu <jsidhu26@myseneca.ca>
2019-03-21 08:25:02 -0400

    group page, group form (create & edit), user group view, search by name

--- src/app/group-form/group-form.component.ts
+++ src/app/group-form/group-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Group } from '../group';
+import { NgForm } from '@angular/forms';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-group-form',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-form.component.css']
 })
 export class GroupFormComponent implements OnInit {
-
-  constructor() { }
+  title = 'group model';
+  group: Group;
+  constructor( private router: Router,
+    private api: ApiService) { 
+    this.group = new Group();
+  }
 
   ngOnInit() {
+  }
+
+  GroupFormSubmit(f: NgForm): void {
+
+    console.log("group name:" + this.group.GROUPNAME);
+    console.log("group privacy: " + this.group.GROUPPRIVACY);
+    console.log("group summary: " + this.group.GROUPSUMMARY);
+
+    //group owner must be the person creating it
+    this.group.GROUPOWNER = document.cookie.split("=")[1];
+
+    //object for creating group
+    var obj = {
+      "GROUPNAME": this.group.GROUPNAME,
+      "GROUPPRIVACY": this.group.GROUPPRIVACY,
+      "GROUPSUMMARY": this.group.GROUPSUMMARY,
+      "GROUPOWNER": this.group.GROUPOWNER
+    };    
+    console.log("obj: (before creation)" + obj);
+    //use the API function to create the group
+    this.api.createGroup(obj).subscribe(res =>{
+      console.log(res);
+    }, err =>{
+      console.log("Error: " + err);
+    });
+    //this.router.navigate(['/user/', document.cookie.split("=")[1]]);
+
   }
 
 }
